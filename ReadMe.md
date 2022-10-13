3385a5fc6ba4f509245487b638917aa9f6e8f73a
SachinGadge13 <gadges14@gmail.com>
2022-10-13 16:16:54 +0530

    Added:OOPs AddressBook System

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+This is OOP-s-AddressBook-System
---
+++ src/com/bridgelabz/addressBook/AddressBook.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressBook;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
